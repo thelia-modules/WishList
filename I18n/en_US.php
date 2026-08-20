ddcb562db2a0fb1a9f6f7c5683541e5ebefcd8b9
Alexandre Nozière <anoziere@openstudio.fr>
2026-08-20 09:25:38 +0200

    feat: translate the wish list service messages

---
+++ I18n/en_US.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'There is no wishlist with this id for this customer' => 'There is no wishlist with this id for this customer',
+    'You already have a wishlist with this name' => 'You already have a wishlist with this name',
+];
